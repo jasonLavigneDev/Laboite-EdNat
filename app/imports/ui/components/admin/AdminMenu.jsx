927d244a4778c58465a5d1f785899348ab8eba6d
Francois AUBEUT <francois.aubeut@make.org>
2021-11-18 11:41:30 +0100

    feat(admin): add structure admin to admin interface

--- app/imports/ui/components/admin/AdminMenu.jsx
+++ app/imports/ui/components/admin/AdminMenu.jsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import { Roles } from 'meteor/alanning:roles';
 import i18n from 'meteor/universe:i18n';
