5c50163ae965569addf11641fe782a9efcfe1283
Francois AUBEUT <francois.aubeut@make.org>
2021-12-06 11:52:18 +0100

    change header & button & card styles

--- app/imports/ui/themes/rizomo/overrides.js
+++ app/imports/ui/themes/rizomo/overrides.js
@@ -1,7 +1,11 @@
 import snackbar from './components/snackbar';
+import card from './components/card';
+import button from './components/button';
 
 const overrides = (palette) => ({
   ...snackbar(palette),
+  ...card,
+  ...button,
 });
 
 export default overrides;
