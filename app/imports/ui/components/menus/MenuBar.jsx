f71b04e5329a23e27b109dc6557a9624f4e4a824
Francois AUBEUT <francois.aubeut@make.org>
2021-12-06 11:52:18 +0100

    feat(theme): change header & button & card styles

--- app/imports/ui/components/menus/MenuBar.jsx
+++ app/imports/ui/components/menus/MenuBar.jsx
@@ -14,6 +14,8 @@ import AppsIcon from '@material-ui/icons/Apps';
 import { useAppContext } from '../../contexts/context';
 import updateDocumentTitle from '../../utils/updateDocumentTitle';
 
+const isRizomo = Meteor.settings.public.theme === 'rizomo';
+
 export const links = [
   {
     path: '/',
@@ -121,7 +123,7 @@ const MenuBar = ({ mobile }) => {
         indicator: classes.indicator,
       }}
       value={currentLink ? currentLink.path : false}
-      indicatorColor="secondary"
+      indicatorColor={isRizomo ? 'primary' : 'secondary'}
       textColor="primary"
       aria-label="menu links"
       variant="scrollable"
