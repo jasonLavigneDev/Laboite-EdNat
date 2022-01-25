878d450c3a06e5d5da0dc67b4fbb42ce4088c539
Francois AUBEUT <contat@mexar.fr>
2022-01-25 15:22:11 +0100

    refactor(css): no structure selected

--- app/imports/ui/components/system/NoStructureSelected.jsx
+++ app/imports/ui/components/system/NoStructureSelected.jsx
@@ -1,39 +1,48 @@
 import React from 'react';
 import i18n from 'meteor/universe:i18n';
 import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 
-const useStyle = makeStyles((theme) => ({
-  title: {
-    textAlign: 'center',
+const useStyles = makeStyles({
+  root: {
+    marginTop: 20,
   },
-  paragraph: {
-    textAlign: 'center',
-    marginTop: 30,
+  actions: {
+    display: 'flex',
+    justifyContent: 'right',
   },
-  link: {
-    color: theme.palette.primary.main,
-    textDecoration: 'none',
-    outline: 'none',
-    marginRight: 25,
-    fontFamily: 'WorkSansBold',
-  },
-}));
+});
 
 const NoStructureSelected = () => {
-  const classes = useStyle();
+  const classes = useStyles();
   return (
-    <>
-      <Typography className={classes.title} variant="h5" color="inherit">
-        {i18n.__('components.NoStructureSelected.noStructure')}
-      </Typography>
-      <Link className={classes.link} to="/profile">
-        <Typography className={classes.paragraph} paragraph color="inherit">
-          {i18n.__('components.NoStructureSelected.selectStructure')}
-        </Typography>
-      </Link>
-    </>
+    <Grid className={classes.root} container justifyContent="center">
+      <Grid item md={4} xs={11}>
+        <Card>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              {i18n.__('components.NoStructureSelected.noStructure')}
+            </Typography>
+            <Typography variant="h6" component="h3">
+              {i18n.__('components.NoStructureSelected.selectStructure')}
+            </Typography>
+          </CardContent>
+          <CardActions className={classes.actions}>
+            <Link to="/profile" className={classes.noUnderline}>
+              <Button variant="contained" color="primary">
+                {i18n.__('components.MainMenu.menuProfileLabel')}
+              </Button>
+            </Link>
+          </CardActions>
+        </Card>
+      </Grid>
+    </Grid>
   );
 };
 
