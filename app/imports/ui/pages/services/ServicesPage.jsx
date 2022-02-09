c10e007bfd2e1849b117d77d5d2d5cbdc018655a
otho <34002285+othomation@users.noreply.github.com>
2022-02-09 09:58:50 +0100

    fix(structures): distinguish publication to match test cases

--- app/imports/ui/pages/services/ServicesPage.jsx
+++ app/imports/ui/pages/services/ServicesPage.jsx
@@ -382,7 +382,7 @@ ServicesPage.defaultProps = {
 export default withTracker(({ match: { path } }) => {
   const [{ structureIds }] = useAppContext();
   const structureMode = path === '/structure';
-  const subName = structureMode ? 'services.structure' : 'services.all';
+  const subName = structureMode ? 'services.structure.ids' : 'services.all';
   const servicesHandle = Meteor.subscribe(subName, structureMode && { structureIds });
   let services;
   if (structureMode) {
