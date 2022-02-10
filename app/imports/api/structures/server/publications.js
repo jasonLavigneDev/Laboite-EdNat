f4ff7a13217bc798592014de817743abd97e8f77
otho <34002285+othomation@users.noreply.github.com>
2022-02-10 12:47:24 +0100

    refactor(structure): extract TreeItem, optimize loading time
    extract tree item, optimize loading time ,enchance modal, add publication, remove error throw in one method

--- app/imports/api/structures/server/publications.js
+++ app/imports/api/structures/server/publications.js
@@ -20,9 +20,27 @@ Meteor.publish('structures.one', function structuresOne() {
 });
 
 // publish structures from a list ids
-Meteor.publish('structures.ids', function structuresids({ ids }) {
+Meteor.publish('structures.ids', function structuresids({ ids } = { ids: [] }) {
   // if (!isActive(this.userId)) {
   //   return this.ready();
   // }
   return Structures.find({ _id: { $in: ids } }, { fields: Structures.publicFields, sort: { name: 1 }, limit: 10000 });
+});
+
+// publish top level structure and possibly childs
+Meteor.publish('structures.top.with.childs', function structuresTopWithChilds({ parentIds } = { parentIds: [] }) {
+  if (!isActive(this.userId)) {
+    return this.ready();
+  }
+
+  return Structures.find(
+    {
+      $or: [{ parentId: null }, { parentId: { $in: parentIds } }],
+    },
+    {
+      fields: Structures.publicFields,
+      sort: { name: 1 },
+      limit: 10000,
+    },
+  );
 });
--- app/imports/ui/pages/admin/AdminStructuresManagementPage.jsx
+++ app/imports/ui/pages/admin/AdminStructuresManagementPage.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 import i18n from 'meteor/universe:i18n';
-
 import { useTracker } from 'meteor/react-meteor-data';
-
+import { _ } from 'meteor/underscore';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardActions from '@material-ui/core/CardActions';
+import Card from '@material-ui/core/Card';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import Dialog from '@material-ui/core/Dialog';
 import Fade from '@material-ui/core/Fade';
 import Backdrop from '@material-ui/core/Backdrop';
 import Container from '@material-ui/core/Container';
-import TreeItem from '@material-ui/lab/TreeItem';
 import TreeView from '@material-ui/lab/TreeView';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
@@ -14,26 +20,24 @@ import Modal from '@material-ui/core/Modal';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import IconButton from '@material-ui/core/IconButton';
-import AddIcon from '@material-ui/icons/Add';
-import CloseIcon from '@material-ui/icons/Close';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
+import ClearIcon from '@material-ui/icons/Clear';
 import Button from '@material-ui/core/Button';
-import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
+import AdminStructureTreeItem from '../../components/admin/AdminStructureTreeItem';
 import Spinner from '../../components/system/Spinner';
 
 import { useObjectState } from '../../utils/hooks';
 import { getTree } from '../../../api/utils';
 import Structures from '../../../api/structures/structures';
 
-const onCreate = ({ name, parentId }) => {
-  Meteor.call('structures.createStructure', { name, parentId: parentId || null }, (error) => {
+const onCreate = ({ name, parentId, updateParentIdsList }) => {
+  Meteor.call('structures.createStructure', { name, parentId: parentId || null }, (error, success) => {
     if (error) {
       msg.error(error.reason || error.details[0].message);
-    } else {
-      msg.success(i18n.__('api.methods.operationSuccessMsg'));
     }
+    msg.success(i18n.__('api.methods.operationSuccessMsg'));
+    if (success) updateParentIdsList({ ids: [parentId] });
   });
 };
 
@@ -70,17 +74,27 @@ const useModalStyles = makeStyles((theme) => ({
 }));
 
 const AdminStructureManagementPage = () => {
+  const [parentIds, setParentIds] = useState([]);
+
+  /** For controlled tree view api */
+  const [expandedIds, setExpandedIds] = useState([]);
+
   const { treeData, loading } = useTracker(() => {
-    const structuresHandle = Meteor.subscribe('structures.all');
+    const structuresHandle = Meteor.subscribe('structures.top.with.childs', { parentIds });
     const isLoading = !structuresHandle.ready();
-    const structures = Structures.find({}, { sort: { name: 1 } }).fetch();
+    const structures = Structures.find(
+      {
+        $or: [{ parentId: null }, { parentId: { $in: parentIds } }],
+      },
+      { sort: { name: 1 } },
+    ).fetch();
     const data = getTree(structures);
 
     return {
       treeData: data,
       loading: isLoading,
     };
-  });
+  }, [parentIds]);
 
   /** Modal utilities  */
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -94,6 +108,13 @@ const AdminStructureManagementPage = () => {
   const setCreateMode = () => setIsEditMode(false);
 
   const modalClasses = useModalStyles();
+
+  const updateParentIdsList = ({ ids }) => {
+    const uniqueList = [...new Set([...parentIds, ...ids])];
+    if (_.isEqual(parentIds.sort(), uniqueList.sort())) return;
+    if (!isEditMode) setExpandedIds([...expandedIds, ...ids]);
+    setParentIds(uniqueList);
+  };
 
   /**
    * SelectedStructure is a context
@@ -110,8 +131,14 @@ const AdminStructureManagementPage = () => {
     if (isEditMode) {
       onEdit({ structureId: _id, parentId: parentId || null, name: structureName });
     } else {
-      onCreate({ name: structureName, parentId: _id === 'root' ? null : _id || null });
+      onCreate({
+        updateParentIdsList,
+        name: structureName,
+        parentId: _id === 'root' ? null : _id || null,
+      });
     }
+
+    closeModal();
   };
 
   const onClickAddBtn = (nodes) => {
@@ -126,55 +153,20 @@ const AdminStructureManagementPage = () => {
     setSelectedStructure(nodes);
   };
 
+  const [isOpenDeleteConfirm, setIsOpenDeleteConfirm] = useState(false);
+
   const onClickDeleteBtn = (nodes) => {
-    if (
-      /** Confirm is temporary */
-      // eslint-disable-next-line no-restricted-globals
-      confirm(i18n.__('pages.AdminStructuresManagementPage.treeView.confirm'))
-    ) {
-      onDelete({ structureId: nodes._id, name: nodes.name, parentId: nodes.parentId });
-    }
+    setSelectedStructure(nodes);
+    setIsOpenDeleteConfirm(true);
   };
 
-  const renderTree = (nodes) => {
-    return (
-      <TreeItem
-        key={nodes._id}
-        nodeId={nodes._id}
-        label={
-          <Box display="flex" p={1}>
-            <Box flexGrow={1}>
-              <p>{nodes.name}</p>
-            </Box>
-            <Box>
-              <IconButton
-                onClick={() => onClickAddBtn(nodes)}
-                title={i18n.__('pages.AdminStructuresManagementPage.treeView.createStructure')}
-              >
-                <AddIcon />
-              </IconButton>
-
-              <IconButton
-                onClick={() => onClickEditBtn(nodes)}
-                title={i18n.__('pages.AdminStructuresManagementPage.treeView.editStructure')}
-              >
-                <EditIcon />
-              </IconButton>
-
-              <IconButton
-                role="button"
-                onClick={() => onClickDeleteBtn(nodes)}
-                title={i18n.__('pages.AdminStructuresManagementPage.treeView.deleteStructure')}
-              >
-                <DeleteIcon />
-              </IconButton>
-            </Box>
-          </Box>
-        }
-      >
-        {Array.isArray(nodes.children) ? nodes.children.map((node) => renderTree(node)) : null}
-      </TreeItem>
-    );
+  const onDeleteConfirm = () => {
+    onDelete({
+      structureId: selectedStructure._id,
+      name: selectedStructure.name,
+      parentId: selectedStructure.parentId,
+    });
+    setIsOpenDeleteConfirm(false);
   };
 
   return (
@@ -184,53 +176,72 @@ const AdminStructureManagementPage = () => {
       ) : (
         <Fade in>
           <Container style={{ overflowX: 'auto' }}>
-            <div
-              style={{
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%,-50%)',
+            <Modal
+              className={modalClasses.modal}
+              open={isModalOpen}
+              onClose={closeModal}
+              closeAfterTransition
+              BackdropComponent={Backdrop}
+              BackdropProps={{
+                timeout: 500,
               }}
             >
-              <Modal
-                className={modalClasses.modal}
-                open={isModalOpen}
-                onClose={closeModal}
-                closeAfterTransition
-                BackdropComponent={Backdrop}
-                BackdropProps={{
-                  timeout: 500,
-                }}
-              >
-                <Fade in={isModalOpen}>
-                  <div className={modalClasses.paper}>
-                    <IconButton
-                      role="button"
-                      style={{ float: 'right' }}
-                      onClick={closeModal}
-                      title={i18n.__('pages.AdminStructuresManagementPage.close')}
-                    >
-                      <CloseIcon />
-                    </IconButton>
+              <Fade in={isModalOpen}>
+                <div className={modalClasses.paper}>
+                  <Card>
+                    <CardHeader
+                      title={i18n.__(
+                        `pages.AdminStructuresManagementPage.treeView.${
+                          isEditMode ? 'editStructure' : 'createStructure'
+                        }`,
+                      )}
+                      action={
+                        <IconButton title={i18n.__('pages.AdminStructuresManagementPage.close')} onClick={closeModal}>
+                          <ClearIcon />
+                        </IconButton>
+                      }
+                    />
                     <form onSubmit={onSubmit}>
-                      <TextField
-                        label={i18n.__('pages.AdminStructuresManagementPage.columnName')}
-                        value={selectedStructure.name}
-                        name="structureName"
-                        fullWidth
-                        onChange={(e) => {
-                          setSelectedStructure({ name: e.target.value });
-                        }}
-                        autoFocus
-                      />
-                      <Button type="submit" fullWidth color="primary">
-                        Valider
-                      </Button>
+                      <CardContent>
+                        <TextField
+                          label={i18n.__('pages.AdminStructuresManagementPage.columnName')}
+                          value={selectedStructure.name}
+                          name="structureName"
+                          fullWidth
+                          onChange={(e) => {
+                            setSelectedStructure({ name: e.target.value });
+                          }}
+                          autoFocus
+                        />
+                      </CardContent>
+                      <CardActions>
+                        <Button onClick={closeModal}>
+                          <Typography>{i18n.__('pages.AdminStructuresManagementPage.cancel')}</Typography>
+                        </Button>
+
+                        <Button type="submit" variant="contained" color="primary">
+                          <Typography>{i18n.__('pages.AdminStructuresManagementPage.submit')}</Typography>
+                        </Button>
+                      </CardActions>
                     </form>
-                  </div>
-                </Fade>
-              </Modal>
-            </div>
+                  </Card>
+                </div>
+              </Fade>
+            </Modal>
+            <Dialog maxWidth="xs" open={isOpenDeleteConfirm}>
+              <DialogTitle>{selectedStructure.name}</DialogTitle>
+              <DialogContent>
+                <Typography>{i18n.__('pages.AdminStructuresManagementPage.treeView.confirm')}</Typography>
+              </DialogContent>
+              <DialogActions>
+                <Button autoFocus onClick={() => setIsOpenDeleteConfirm(false)}>
+                  <Typography>{i18n.__('pages.AdminStructuresManagementPage.cancel')}</Typography>
+                </Button>
+                <Button onClick={onDeleteConfirm} variant="contained" color="primary">
+                  <Typography>{i18n.__('pages.AdminStructuresManagementPage.submit')}</Typography>
+                </Button>
+              </DialogActions>
+            </Dialog>
             <TreeView
               aria-label={i18n.__('pages.AdminStructuresManagementPage.list')}
               defaultCollapseIcon={<ExpandMoreIcon />}
@@ -240,8 +251,21 @@ const AdminStructureManagementPage = () => {
                 flexGrow: 1,
                 overflowY: 'auto',
               }}
+              expanded={expandedIds}
+              onNodeToggle={(event, nodesIds) => {
+                setExpandedIds(nodesIds);
+              }}
             >
-              {treeData.map(renderTree)}
+              {treeData.map((nodes) => (
+                <AdminStructureTreeItem
+                  key={nodes._id}
+                  nodes={nodes}
+                  onClickAddBtn={onClickAddBtn}
+                  onClickEditBtn={onClickEditBtn}
+                  onClickDeleteBtn={onClickDeleteBtn}
+                  updateParentIdsList={updateParentIdsList}
+                />
+              ))}
             </TreeView>
           </Container>
         </Fade>
