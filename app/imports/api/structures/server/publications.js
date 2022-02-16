d76b643ddee70be511613952e74811adc82c5b55
otho <34002285+othomation@users.noreply.github.com>
2022-02-16 10:41:13 +0100

    fix(publication structure): remove unused pub

--- app/imports/api/structures/server/publications.js
+++ app/imports/api/structures/server/publications.js
@@ -1,6 +1,4 @@
-import { Roles } from 'meteor/alanning:roles';
 import { Meteor } from 'meteor/meteor';
-import i18n from 'meteor/universe:i18n';
 import { isActive } from '../../utils';
 import Structures from '../structures';
 
@@ -45,52 +43,4 @@ Meteor.publish('structures.top.with.childs', function structuresTopWithChilds({ 
       limit: 10000,
     },
   );
-});
-
-Meteor.publish('structures.with.childs', async function structuresWithChilds({ structureId }) {
-  if (!isActive(this.userId)) {
-    return this.ready();
-  }
-
-  const structure = Structures.find({ _id: structureId });
-  if (!structure) {
-    throw new Meteor.Error(
-      'api.structures.updateStructure.unknownStructure',
-      i18n.__('api.structures.unknownStructure'),
-    );
-  }
-
-  const authorized = isActive(this.userId) && Roles.userIsInRole(this.userId, 'admin');
-
-  if (!authorized) {
-    throw new Meteor.Error('api.structures.updateStructure.notPermitted', i18n.__('api.users.notPermitted'));
-  }
-
-  const data = await Structures.rawCollection().aggregate([
-    {
-      $graphLookup: {
-        from: 'collection',
-        startWith: '$parentId',
-        connectFromField: 'parentId',
-        connectToField: '_id',
-        as: 'children',
-      },
-    },
-    { $match: { _id: 'EviKfwqdFZcjEWFdz' } },
-    {
-      $addFields: {
-        children: {
-          $reverseArray: {
-            $map: {
-              input: '$children',
-              as: 't',
-              in: { _id: '$$t._id' },
-            },
-          },
-        },
-      },
-    },
-  ]);
-
-  return data;
 });
