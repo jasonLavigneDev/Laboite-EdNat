07965195330ff96a35b927008d7e66b5724961e9
Francois AUBEUT <contat@mexar.fr>
2022-02-28 16:11:49 +0100

    test(help): add test for help api

---
+++ app/imports/api/helps/server/factories.js
@@ -0,0 +1,12 @@
+import { Factory } from 'meteor/dburles:factory';
+import { Random } from 'meteor/random';
+import faker from 'faker';
+
+import Helps from '../helps';
+
+Factory.define('help', Helps, {
+  title: () => Random.id(),
+  description: faker.lorem.sentence().substring(0, 80),
+  type: 0,
+  content: faker.lorem.sentence(),
+});
