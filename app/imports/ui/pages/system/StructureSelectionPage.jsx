95c2e35237f52f275bae8363579019fd97c50a90
otho <34002285+othomation@users.noreply.github.com>
2022-03-10 18:40:25 +0100

    feat(structure selection): use select and detail

---
+++ app/imports/ui/pages/system/StructureSelectionPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import StructureSelection from '../../components/profilePage/StructureSelection';
+
+const StructureSelectionPage = () => {
+  return (
+    <Box>
+      <StructureSelection />
+    </Box>
+  );
+};
+
+export default StructureSelectionPage;
