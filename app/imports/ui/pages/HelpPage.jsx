b4e38cb3ce3933a179cfcdeecca6f70688cae9ec
Francois AUBEUT <contat@mexar.fr>
2022-03-14 21:35:27 +0100

    feat(help): display help in categories

--- app/imports/ui/pages/HelpPage.jsx
+++ app/imports/ui/pages/HelpPage.jsx
@@ -8,6 +8,9 @@ import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Modal from '@material-ui/core/Modal';
@@ -16,14 +19,22 @@ import CancelIcon from '@material-ui/icons/CancelOutlined';
 import Screencast from '../components/screencast/Screencast';
 import { useAppContext } from '../contexts/context';
 import Helps from '../../api/helps/helps';
+import { useZoneStyles } from '../components/personalspace/PersonalZone';
 
 function HelpPage() {
   const [openScreencast, setScreencastModal] = useState(false);
   const [{ isMobile }] = useAppContext();
   const helps = useTracker(() => {
     const subs = Meteor.subscribe('helps.all');
-    const ready = subs.ready();
-    return ready ? Helps.find({}, { sort: { title: 1 }, limit: 10000 }) : [];
+    const helpsData = Helps.find({}, { sort: { title: 1 }, limit: 10000 });
+    const categorieData = new Set([]);
+    helpsData.forEach(({ category }) => categorieData.add(category));
+    return subs.ready()
+      ? [...categorieData].map((category) => ({
+          name: category,
+          items: Helps.find({ category }, { sort: { title: 1 }, limit: 10000 }),
+        }))
+      : [];
   });
 
   const useStyles = makeStyles((theme) => ({
@@ -78,6 +89,7 @@ function HelpPage() {
   }));
 
   const classes = useStyles();
+  const zoneClasses = useZoneStyles();
   const [link, setLink] = useState('');
 
   const openItem = (item) => {
@@ -96,29 +108,51 @@ function HelpPage() {
           <Grid item md={12}>
             <Typography variant={isMobile ? 'h5' : 'h4'}>{i18n.__('pages.HelpPage.title')}</Typography>
           </Grid>
-          {helps.map(({ title, type, content, description }) => (
-            <Grid item xs={12} md={6} lg={4} key={title}>
-              <Card className={classes.card}>
-                <CardHeader title={title} />
+          {helps.map((category) => (
+            <Grid item md={12}>
+              <Accordion key={category.name} className={zoneClasses.expansionpanel} expanded>
+                <AccordionSummary
+                  aria-controls={`zone-${category.name}`}
+                  id={`expand-${category.name}`}
+                  zoneClasses={{
+                    expanded: zoneClasses.expansionpanelsummaryexpanded,
+                    content: zoneClasses.expansionpanelsummarycontent,
+                  }}
+                >
+                  <Typography variant="h5" color="primary" className={zoneClasses.zone}>
+                    {category.name}
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Grid container spacing={4} className={classes.grid}>
+                    {category.items.map(({ title, type, content, description }) => (
+                      <Grid item xs={12} md={6} lg={4} key={title}>
+                        <Card className={classes.card}>
+                          <CardHeader title={title} />
 
-                <CardContent>
-                  {!!description && (
-                    <Typography color="textSecondary" gutterBottom>
-                      {description}
-                    </Typography>
-                  )}
-                </CardContent>
-                <CardContent>
-                  <Button
-                    startIcon={<ExitToAppIcon />}
-                    className={classes.buttonText}
-                    size="large"
-                    onClick={() => openItem({ title, type, content, description })}
-                  >
-                    {i18n.__('pages.HelpPage.tutoLabel')}
-                  </Button>
-                </CardContent>
-              </Card>
+                          <CardContent>
+                            {!!description && (
+                              <Typography color="textSecondary" gutterBottom>
+                                {description}
+                              </Typography>
+                            )}
+                          </CardContent>
+                          <CardContent>
+                            <Button
+                              startIcon={<ExitToAppIcon />}
+                              className={classes.buttonText}
+                              size="large"
+                              onClick={() => openItem({ title, type, content, description })}
+                            >
+                              {i18n.__('pages.HelpPage.tutoLabel')}
+                            </Button>
+                          </CardContent>
+                        </Card>
+                      </Grid>
+                    ))}
+                  </Grid>
+                </AccordionDetails>
+              </Accordion>
             </Grid>
           ))}
         </Grid>
