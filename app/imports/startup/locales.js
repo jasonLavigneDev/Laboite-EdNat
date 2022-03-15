8889e7d5ea7a89cdf5e995806c59bbef973f2b80
Francois AUBEUT <contat@mexar.fr>
2022-03-15 23:07:18 +0100

    feat(env): add two env variables for i18n languages

--- app/imports/startup/locales.js
+++ app/imports/startup/locales.js
@@ -1,2 +1,16 @@
-import '../../i18n/en.i18n.json';
-import '../../i18n/fr.i18n.json';
+import i18n from 'meteor/universe:i18n';
+
+const { I18N_EN, I18N_FR } = process.env;
+
+if (I18N_FR) {
+  i18n.addTranslations('fr', I18N_FR);
+} else {
+  const fr = import('../../i18n/fr.i18n.json');
+  i18n.addTranslations('fr', fr);
+}
+if (I18N_EN) {
+  i18n.addTranslations('en', I18N_EN);
+} else {
+  const en = import('../../i18n/en.i18n.json');
+  i18n.addTranslations('en', en);
+}
