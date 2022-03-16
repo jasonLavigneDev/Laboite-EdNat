1d1bc5d48ccf632789dbae07122359c42da49f76
Francois AUBEUT <contat@mexar.fr>
2022-03-16 23:40:16 +0100

    feat(help): add help on offline page

--- app/imports/api/helps/server/publications.js
+++ app/imports/api/helps/server/publications.js
@@ -1,11 +1,7 @@
 import { Meteor } from 'meteor/meteor';
-import { isActive } from '../../utils';
 import Helps from '../helps';
 
 // publish all categories
 Meteor.publish('helps.all', function helpsAll() {
-  if (!isActive(this.userId)) {
-    return this.ready();
-  }
   return Helps.find({}, { fields: Helps.publicFields, sort: { title: 1 }, limit: 10000 });
 });
