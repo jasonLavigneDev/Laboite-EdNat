8b530410491aa98da455a2d8c577ffebebdb2849
Othmane ROGLER <othmane.rogler@apitech.fr>
2022-04-05 13:47:13 +0200

    fix(structure pub): fix short circuit on structure one

--- app/imports/api/structures/server/publications.js
+++ app/imports/api/structures/server/publications.js
@@ -15,8 +15,11 @@ Meteor.publish('structures.one', function structuresOne({ _id } = { _id: null })
   if (!isActive(this.userId)) {
     return this.ready();
   }
-  const user = Meteor.users.findOne({ _id: _id || this.userId });
-  return Structures.find({ _id: user.structure }, { fields: Structures.publicFields, sort: { name: 1 }, limit: 1 });
+  const user = Meteor.users.findOne({ _id: this.userId });
+  return Structures.find(
+    { _id: _id || user.structure },
+    { fields: Structures.publicFields, sort: { name: 1 }, limit: 1 },
+  );
 });
 
 // publish structures from a list ids
