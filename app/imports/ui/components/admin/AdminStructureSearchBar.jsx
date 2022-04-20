668d1674956ea896e3e7acca249c94d8d07593be
Othmane ROGLER <othmane.rogler@apitech.fr>
2022-04-20 15:54:41 +0200

    feat(structure ui): app level admin can manage multi level structures
    * Add a Tree Item component
    * Add a SearchBar component
    * Add a Tree View component
    * Add CRUD actions to the treeView
    * Add getTree utility function to have a tree shaped data from a flat one
    * Add structures.top.with.childs and structures.top.with.direct.parent publications for the tree data and search usages

---
+++ app/imports/ui/components/admin/AdminStructureSearchBar.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import i18n from 'meteor/universe:i18n';
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import SearchIcon from '@material-ui/icons/Search';
+import IconButton from '@material-ui/core/IconButton';
+import ClearIcon from '@material-ui/icons/Clear';
+import InputAdornment from '@material-ui/core/InputAdornment';
+
+const AdminStructureSearchBar = (props) => {
+  const { searchValue, setSearchText, resetSearchText, resetFilter } = props;
+  return (
+    <Box sx={{ p: 0.5, pb: 0 }}>
+      <TextField
+        fullWidth
+        variant="outlined"
+        value={searchValue}
+        onChange={(e) => {
+          setSearchText(e.target.value);
+          if (e.target.value.length < 1) resetFilter();
+        }}
+        placeholder={i18n.__('components.AdminStructureSearchBar.filterByName')}
+        title={i18n.__('components.AdminStructureSearchBar.filterByName')}
+        InputProps={{
+          type: 'text',
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+          endAdornment: (
+            <IconButton
+              title={i18n.__('components.AdminStructureSearchBar.clear')}
+              aria-label={i18n.__('components.AdminStructureSearchBar.clear')}
+              size="small"
+              style={{ visibility: searchValue ? 'visible' : 'hidden' }}
+              onClick={() => {
+                resetSearchText();
+                resetFilter();
+              }}
+            >
+              <ClearIcon fontSize="small" />
+            </IconButton>
+          ),
+        }}
+      />
+    </Box>
+  );
+};
+
+AdminStructureSearchBar.propTypes = {
+  searchValue: PropTypes.string.isRequired,
+  setSearchText: PropTypes.func.isRequired,
+  resetSearchText: PropTypes.func.isRequired,
+  resetFilter: PropTypes.func.isRequired,
+};
+
+export default AdminStructureSearchBar;
---
+++ app/imports/ui/components/admin/AdminStructureTreeItem.jsx
@@ -0,0 +1,132 @@
+import React from 'react';
+import i18n from 'meteor/universe:i18n';
+
+import TreeItem from '@material-ui/lab/TreeItem';
+import PropTypes from 'prop-types';
+import Box from '@material-ui/core/Box';
+import IconButton from '@material-ui/core/IconButton';
+import AddBox from '@material-ui/icons/AddBox';
+import DeleteIcon from '@material-ui/icons/DeleteOutline';
+import EditIcon from '@material-ui/icons/Edit';
+import { withStyles, alpha, makeStyles } from '@material-ui/core/styles';
+import Tooltip from '@material-ui/core/Tooltip';
+import Typography from '@material-ui/core/Typography';
+
+const StyledTreeItem = withStyles((theme) => ({
+  iconContainer: {
+    '& .close': {
+      opacity: 0.3,
+    },
+  },
+  group: {
+    marginLeft: 10,
+    paddingLeft: 18,
+    borderLeft: `1px dashed ${alpha(theme.palette.text.primary, 0.4)}`,
+  },
+}))((props) => <TreeItem {...props} />);
+
+const useStyles = makeStyles(() => ({
+  name: {
+    alignItems: 'center',
+    display: 'flex',
+  },
+}));
+
+const AdminStructureTreeItem = ({
+  nodes,
+  onClickAddBtn,
+  onClickEditBtn,
+  onClickDeleteBtn,
+  updateParentIdsList,
+  selectedId,
+}) => {
+  const { _id: id, name, children, childrenIds } = nodes;
+  const classes = useStyles();
+  const hasChildren = childrenIds.length > 0;
+
+  return (
+    <StyledTreeItem
+      key={id}
+      nodeId={id}
+      onLabelClick={() => hasChildren && updateParentIdsList({ ids: [id] })}
+      onIconClick={() => hasChildren && updateParentIdsList({ ids: [id] })}
+      label={
+        <Box display="flex">
+          <Box flexGrow={1} className={classes.name}>
+            <div>
+              <Typography>{name}</Typography>
+            </div>
+          </Box>
+          <Box>
+            {onClickAddBtn && (
+              <Tooltip title={i18n.__('components.AdminStructureTreeItem.actions.addStructure')}>
+                <span>
+                  <IconButton onClick={() => onClickAddBtn(nodes)}>
+                    <AddBox />
+                  </IconButton>
+                </span>
+              </Tooltip>
+            )}
+            {onClickEditBtn && (
+              <Tooltip title={i18n.__('components.AdminStructureTreeItem.actions.editStructure')}>
+                <span>
+                  <IconButton onClick={() => onClickEditBtn(nodes)}>
+                    <EditIcon />
+                  </IconButton>
+                </span>
+              </Tooltip>
+            )}
+
+            {onClickDeleteBtn && (
+              <Tooltip
+                title={i18n.__(
+                  `components.AdminStructureTreeItem.actions.deleteStructure${
+                    hasChildren ? 'ImpossibleHasChildren' : ''
+                  }`,
+                )}
+              >
+                <span>
+                  <IconButton disabled={hasChildren} role="button" onClick={() => onClickDeleteBtn(nodes)}>
+                    <DeleteIcon />
+                  </IconButton>
+                </span>
+              </Tooltip>
+            )}
+          </Box>
+        </Box>
+      }
+    >
+      {Array.isArray(children) ? (
+        children.map((node) => (
+          <AdminStructureTreeItem
+            key={node._id}
+            nodes={node}
+            onClickAddBtn={onClickAddBtn}
+            onClickEditBtn={onClickEditBtn}
+            onClickDeleteBtn={onClickDeleteBtn}
+            updateParentIdsList={updateParentIdsList}
+            selectedId={selectedId}
+          />
+        ))
+      ) : hasChildren ? (
+        <span>&nbsp;</span>
+      ) : null}
+    </StyledTreeItem>
+  );
+};
+
+AdminStructureTreeItem.propTypes = {
+  nodes: PropTypes.objectOf(PropTypes.any).isRequired,
+  onClickAddBtn: PropTypes.func,
+  onClickEditBtn: PropTypes.func,
+  onClickDeleteBtn: PropTypes.func,
+  updateParentIdsList: PropTypes.func.isRequired,
+  selectedId: PropTypes.string.isRequired,
+};
+
+AdminStructureTreeItem.defaultProps = {
+  onClickAddBtn: undefined,
+  onClickEditBtn: undefined,
+  onClickDeleteBtn: undefined,
+};
+export default AdminStructureTreeItem;
