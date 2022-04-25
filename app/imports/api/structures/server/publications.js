2a6628ef3e8ef874cf8c81f5215671891fe58223
Othmane ROGLER <othmane.rogler@apitech.fr>
2022-04-25 14:41:19 +0200

    feat(structures): structure level admin user can manage sub structures
    * Update "structures.with.top.childs" publication callback signature
    * Add page and menu for sub strucures management (/admin/substructures)
    * Add i18n

--- app/imports/api/structures/server/publications.js
+++ app/imports/api/structures/server/publications.js
@@ -42,7 +42,9 @@ Meteor.publish('structures.ids', function structuresids({ ids } = { ids: [] }) {
 
 FindFromPublication.publish(
   'structures.top.with.childs',
-  function structuresTopWithChilds({ parentIds, searchText } = { parentIds: [], searchText: '' }) {
+  function structuresTopWithChilds(
+    { parentIds, searchText, getTopLevelStructures } = { parentIds: [], searchText: '', getTopLevelStructures: true },
+  ) {
     if (!isActive(this.userId)) {
       return this.ready();
     }
@@ -59,7 +61,9 @@ FindFromPublication.publish(
       }, []);
       query._id = { $in: ids };
     } else {
-      query.$or = [{ parentId: null }, { parentId: { $in: parentIds } }];
+      query.$or = getTopLevelStructures
+        ? [{ parentId: null }, { parentId: { $in: parentIds } }]
+        : [{ parentId: { $in: parentIds } }];
     }
 
     return Structures.find(query, {
