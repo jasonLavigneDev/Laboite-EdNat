3f130fcb5861f6537564fc0a15ce56fe043af0d9
Othmane ROGLER <othmane.rogler@apitech.fr>
2022-04-26 15:47:53 +0200

    feat(structure): add a structure select component and related hook
    * add "StructureSelect" component (it use "CustomSelect")
    * add "useAdminSelectedStructure" react hook
    * add i18n
    * add "structure.with.all.childs" publication

--- app/imports/api/structures/hooks.js
+++ app/imports/api/structures/hooks.js
@@ -1,5 +1,6 @@
 import { useTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
+import { useEffect } from 'react';
 import Structures from './structures';
 import { useAppContext } from '../../ui/contexts/context';
 
@@ -16,3 +17,39 @@ export const useStructure = (_id) => {
 };
 
 export const getStructure = (_id) => Structures.findOne({ _id });
+
+/**
+ * - Hook used to manage a selected structure
+ *
+ * - Use cases are mainly to be used with `<StructureSelect />` component
+ */
+export const useAdminSelectedStructure = ({
+  /** Should be a state */
+  selectedStructureId,
+  /** Should be the state mutato */
+  setSelectedStructureId,
+}) => {
+  const subName = 'structures.with.all.childs';
+
+  // Grab current user structure
+  const currentUserStructure = useStructure();
+
+  // Use this as a starting point and update it when user change structure
+  useEffect(() => {
+    if (currentUserStructure && currentUserStructure._id) setSelectedStructureId(currentUserStructure._id);
+  }, [currentUserStructure]);
+
+  return useTracker(() => {
+    const selectedStructure = getStructure(selectedStructureId);
+
+    const handle = Meteor.subscribe(subName, { structureId: currentUserStructure ? currentUserStructure._id : '' });
+    const loading = !handle.ready();
+    const data = Structures.findFromPublication(subName).fetch();
+
+    return {
+      selectedStructure,
+      loading,
+      structures: data,
+    };
+  }, [selectedStructureId]);
+};
