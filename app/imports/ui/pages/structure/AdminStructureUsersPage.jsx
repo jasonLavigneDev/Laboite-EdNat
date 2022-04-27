8ad63a72c2c620b7862c26688f09431c597022df
Francois AUBEUT <contat@mexar.fr>
2022-04-27 14:24:13 +0200

    fix(duplicate): remove code clones

--- app/imports/ui/pages/structure/AdminStructureUsersPage.jsx
+++ app/imports/ui/pages/structure/AdminStructureUsersPage.jsx
@@ -99,10 +99,9 @@ const AdminStructureUsersPage = ({ match: { path } }) => {
   const searchRef = useRef();
   const updateSearch = (e) => setSearch(e.target.value);
   const resetSearch = () => setSearch('');
+
   useEffect(() => {
     if (searchRef.current) searchRef.current.value = search;
-  }, [search]);
-  useEffect(() => {
     if (page !== 1) {
       changePage(1);
     }
