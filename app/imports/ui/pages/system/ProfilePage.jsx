115df2d83406cd1d318edd38fe0917d57b2293c8
Othmane ROGLER <othmane.rogler@apitech.fr>
2022-05-10 14:15:51 +0200

    fix(profile ui): use bold text for attached structure name

--- app/imports/ui/pages/system/ProfilePage.jsx
+++ app/imports/ui/pages/system/ProfilePage.jsx
@@ -578,9 +578,13 @@ const ProfilePage = () => {
               <Grid item className={classes.maxWidth}>
                 <FormControl variant="filled" className={classes.formControl} fullWidth>
                   <Typography>
-                    {selectedStructure && selectedStructure.name
-                      ? `${i18n.__('pages.ProfilePage.currentStructure')} ${selectedStructure.name}`
-                      : i18n.__('pages.ProfilePage.noCurrentStructure')}
+                    {selectedStructure && selectedStructure.name ? (
+                      <span>
+                        {i18n.__('pages.ProfilePage.currentStructure')} <b>{selectedStructure.name}</b>
+                      </span>
+                    ) : (
+                      <span>{i18n.__('pages.ProfilePage.noCurrentStructure')}</span>
+                    )}
                   </Typography>
                   <AutoComplete
                     options={flatData}
