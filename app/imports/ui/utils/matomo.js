290b45065abd3675b9fc62b0e10219df82aa7a18
Francois AUBEUT <contat@mexar.fr>
2022-05-17 15:16:41 +0200

    feat(matomo): add doc & fix no matomo mode
    feat(matomo): add readme

--- app/imports/ui/utils/matomo.js
+++ app/imports/ui/utils/matomo.js
@@ -1,31 +1,34 @@
 import { createInstance, useMatomo } from '@datapunt/matomo-tracker-react';
 import { useEffect } from 'react';
 
-const {
-  matomo: { urlBase, siteId },
-} = Meteor.settings.public;
+const { matomo = {} } = Meteor.settings.public;
 
-export const instance = createInstance({
-  urlBase,
-  siteId,
-  //   userId: 'UID76903202', // optional, default value: `undefined`.
-  trackerUrl: `${urlBase}/matomo.php`, // optional, default value: `${urlBase}matomo.php`
-  srcUrl: `${urlBase}/matomo.js`, // optional, default value: `${urlBase}matomo.js`
-  disabled: false, // optional, false by default. Makes all tracking calls no-ops if set to true.
-  heartBeat: {
-    // optional, enabled by default
-    active: true, // optional, default value: true
-    seconds: 10, // optional, default value: `15
-  },
-  linkTracking: false, // optional, default value: true
-  configurations: {
-    // optional, default value: {}
-    // any valid matomo configuration, all below are optional
-    // disableCookies: true,
-    // setSecureCookie: true,
-    // setRequestMethod: 'POST',
-  },
-});
+const { urlBase, siteId } = matomo;
+
+export const instance =
+  urlBase && siteId
+    ? createInstance({
+        urlBase,
+        siteId,
+        //   userId: 'UID76903202', // optional, default value: `undefined`.
+        // trackerUrl: `${urlBase}/matomo.php`, // optional, default value: `${urlBase}matomo.php`
+        // srcUrl: `${urlBase}/matomo.js`, // optional, default value: `${urlBase}matomo.js`
+        disabled: false, // optional, false by default. Makes all tracking calls no-ops if set to true.
+        heartBeat: {
+          // optional, enabled by default
+          active: true, // optional, default value: true
+          seconds: 10, // optional, default value: `15
+        },
+        linkTracking: false, // optional, default value: true
+        configurations: {
+          // optional, default value: {}
+          // any valid matomo configuration, all below are optional
+          // disableCookies: true,
+          // setSecureCookie: true,
+          // setRequestMethod: 'POST',
+        },
+      })
+    : null;
 
 export const usePageTracking = (args) => {
   const { trackPageView } = useMatomo();
