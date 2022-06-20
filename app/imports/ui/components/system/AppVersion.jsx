dbb0cfc582d97a88f387ce4402de9c7584834b15
Francois AUBEUT <contat@mexar.fr>
2022-06-20 18:47:48 +0200

    feat(442): add firefox storage request on login 20220620-1847

--- app/imports/ui/components/system/AppVersion.jsx
+++ app/imports/ui/components/system/AppVersion.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import i18n from 'meteor/universe:i18n';
 
 import PackageJSON from '../../../../package.json';
+import { checkAccessAndLogin } from '../../pages/system/SignIn';
+import { useAppContext } from '../../contexts/context';
 
 const { version } = PackageJSON;
-const AppVersion = () => (
-  <span
-    style={{
-      opacity: 0.5,
-    }}
-  >
-    {i18n.__('components.AppVersion.title')} {version}
-  </span>
-);
+const AppVersion = () => {
+  const [{ isIframed }] = useAppContext();
+  return (
+    <button
+      style={{
+        opacity: 0.5,
+        border: 'none',
+        background: 'transparent',
+      }}
+      type="button"
+      onClick={() => checkAccessAndLogin(isIframed)}
+    >
+      {i18n.__('components.AppVersion.title')} {version}
+    </button>
+  );
+};
 
 export default AppVersion;
