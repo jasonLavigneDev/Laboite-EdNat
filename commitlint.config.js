34d11032d2a5014dfefd0e3e38b30fc3f8de6581
Daniel Dehennin <daniel.dehennin@ac-dijon.fr>
2023-01-06 11:11:20 +0100

    ci(commitlint): don't allow `type` and `scope` starting with digit
    We don't want message subject like
    
        fix(341): foo
    
    The scope must be a descriptive name.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,10 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    parserPreset: {
+        parserOpts: {
+            headerPattern: /^([^0-9]\w*)(?:\(([^0-9].*)\))?!?: (.*)$/,
+        }
+    },
     rules: {
         'body-max-line-length': [2, 'always', 120],
         'footer-max-line-length': [2, 'always', 120],
