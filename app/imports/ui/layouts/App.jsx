ca5c2f56daefdfcff759f45a936627a484499e8d
Octanium <octaniumcontact@gmail.com>
2023-01-19 09:52:13 +0100

    refactor: move LocalizationLayout to HOC
    Signed-off-by: Octanium <octaniumcontact@gmail.com>

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -65,28 +65,26 @@ function App() {
       ) : (
         <Suspense fallback={<Spinner full />}>
           <CssBaseline />
-          <LocalizationLayout>
-            <Switch>
-              <PublicRoute exact path="/signin" component={SignLayout} {...state} />
-              {useKeycloak ? null : <PublicRoute exact path="/signup" component={SignLayout} {...state} />}
-              {externalBlog || !enableBlog ? null : <Route exact path="/public/" component={PublishersPage} />}
-              {externalBlog || !enableBlog ? null : <Route exact path="/public/:userId" component={ArticlesPage} />}
-              {externalBlog || !enableBlog ? null : (
-                <Route exact path="/public/:userId/:slug" component={PublicArticleDetailsPage} />
-              )}
-              <ProtectedRoute exact path="/logout" component={Logout} {...state} />
-              <Route exact path="/legal/:legalKey" component={LegalPage} />
-              {!userId && <Route exact path="/contact" component={SignLayout} {...state} />}
-              <ProtectedRoute
-                path="/admin"
-                component={AdminLayout}
-                userId={userId}
-                loadingUser={loadingUser}
-                {...state}
-              />
-              <ProtectedRoute path="/" component={MainLayout} {...state} />
-            </Switch>
-          </LocalizationLayout>
+          <Switch>
+            <PublicRoute exact path="/signin" component={SignLayout} {...state} />
+            {useKeycloak ? null : <PublicRoute exact path="/signup" component={SignLayout} {...state} />}
+            {externalBlog || !enableBlog ? null : <Route exact path="/public/" component={PublishersPage} />}
+            {externalBlog || !enableBlog ? null : <Route exact path="/public/:userId" component={ArticlesPage} />}
+            {externalBlog || !enableBlog ? null : (
+              <Route exact path="/public/:userId/:slug" component={PublicArticleDetailsPage} />
+            )}
+            <ProtectedRoute exact path="/logout" component={Logout} {...state} />
+            <Route exact path="/legal/:legalKey" component={LegalPage} />
+            {!userId && <Route exact path="/contact" component={SignLayout} {...state} />}
+            <ProtectedRoute
+              path="/admin"
+              component={AdminLayout}
+              userId={userId}
+              loadingUser={loadingUser}
+              {...state}
+            />
+            <ProtectedRoute path="/" component={MainLayout} {...state} />
+          </Switch>
           <MsgHandler />
           {!!minioEndPoint && <UploaderNotifier />}
         </Suspense>
@@ -99,11 +97,13 @@ export default () => (
   <MatomoProvider value={instance}>
     <CacheProvider value={muiCache}>
       <ThemeProvider theme={lightTheme}>
-        <BrowserRouter>
-          <DynamicStore>
-            <App />
-          </DynamicStore>
-        </BrowserRouter>
+        <LocalizationLayout>
+          <BrowserRouter>
+            <DynamicStore>
+              <App />
+            </DynamicStore>
+          </BrowserRouter>
+        </LocalizationLayout>
       </ThemeProvider>
     </CacheProvider>
   </MatomoProvider>
