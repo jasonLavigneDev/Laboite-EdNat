613a888e036bcab38a1d174a76e5dd9d9e7adab9
Vincent Fontesse <vincent.fontesse@ac-dijon.fr>
2023-05-11 09:53:11 +0200

    feat(logs): add server log error in migration methods

--- app/imports/api/migration/server/methods.js
+++ app/imports/api/migration/server/methods.js
@@ -4,12 +4,18 @@ import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import { _ } from 'meteor/underscore';
 import i18n from 'meteor/universe:i18n';
 import { Migrations } from 'meteor/percolate:migrations';
+import logServer, { levels, scopes } from '../../logging';
 
 export const getVersion = new ValidatedMethod({
   name: 'migration.getVersion',
   validate: null,
   run() {
     if (!this.userId) {
+      logServer(
+        `MIGRATION - METHOD - METEOR ERROR - getVersion - ${i18n.__('api.users.mustBeLoggedIn')}`,
+        levels.VERBOSE,
+        scopes.SYSTEM,
+      );
       throw new Meteor.Error('api.migration.getVersion.mustBeLoggedIn', i18n.__('api.users.mustBeLoggedIn'));
     }
     return Migrations.getVersion();
