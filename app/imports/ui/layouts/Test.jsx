d459b5347aefceef1c48446f4d01a2626c03f5c2
Vincent Fontesse <vincent.fontesse@ac-dijon.fr>
2023-05-25 15:43:23 +0200

    feat(style): add style in test page

--- app/imports/ui/layouts/Test.jsx
+++ app/imports/ui/layouts/Test.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import Paper from '@mui/material/Paper';
+import Chip from '@mui/material/Chip';
 import { useAppContext } from '../contexts/context';
 
 export const GlobalInfoTest = () => {
@@ -69,20 +71,41 @@ export const GlobalInfoTest = () => {
           Recuperer les messages dans le langage navigateur et non expiré
         </button>
       </div>
-      <div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '99vw',
+        }}
+      >
         <h1>Liste des messages en base</h1>
         {messages.map((info) => (
-          <div key={info._id} style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
-            <p>Crée le : {info.createdAt.toLocaleDateString()}</p>
-            <p>Message : {info.content}</p>
+          <Paper
+            key={info._id}
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              width: '80%',
+              alignItems: 'start',
+              marginBottom: '1vh',
+              padding: '2vh 10vw',
+            }}
+          >
+            <p style={{ height: '1vh' }}>Crée le : {info.createdAt.toLocaleDateString()}</p>
             <p>Expire le :{info.expirationDate.toLocaleDateString()}</p>
-            <button style={{ height: '30px' }} type="button">
-              Modifier le message
-            </button>
-            <button style={{ height: '30px' }} type="button" onClick={() => deleteMessage(info._id)}>
-              Supprimer le message
-            </button>
-          </div>
+            <Chip label="Ma super structure" color="primary" />
+            <p>Message : {info.content}</p>
+            <div style={{ display: 'flex', alignSelf: 'center', justifyContent: 'space-between', width: '30%' }}>
+              <button style={{ height: '30px' }} type="button">
+                Modifier le message
+              </button>
+              <button style={{ height: '30px' }} type="button" onClick={() => deleteMessage(info._id)}>
+                Supprimer le message
+              </button>
+            </div>
+          </Paper>
         ))}
       </div>
     </div>
