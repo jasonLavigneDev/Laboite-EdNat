8b3d693d701ed14dd3d7bd2f4e6443752cc96ab6
Octanium <octaniumcontact@gmail.com>
2023-09-27 08:53:51 +0200

    feat: make widget retro compatible
    Signed-off-by: Octanium <octaniumcontact@gmail.com>

--- app/private/widget/script.js
+++ app/private/widget/script.js
@@ -1,3 +1,17 @@
 console.warn(
-  'This SNAP widget scrip is deprecated. See https://gitlab.mim-libre.fr/rizomo/la-pastille to use newer versions',
+  'This SNAP widget script is deprecated. See https://gitlab.mim-libre.fr/rizomo/la-pastille to use newer versions',
 );
+
+const script = document.createElement('script');
+script.src = '{{script}}';
+script.defer = 'true';
+script.type = 'application/javascript';
+
+customElements.whenDefined('snap-widget').then(() => {
+  const widget = document.createElement('div');
+  widget.innerHTML = `<snap-widget url="{{url}}"></snap-widget>`;
+
+  document.body.append(widget);
+});
+
+document.head.appendChild(script);
